856acb79dd5458c5f77d3682656dfd3dcd060393
hyoinkangg <rkdgydls1234@gmail.com>
2024-05-06 08:25:54 +0900

    App과 Chatroom 분리

---
+++ src/Chat/Chat.js
@@ -0,0 +1,81 @@
+import './Chat.css'
+import { useState ,useRef, useEffect } from 'react';
+import Header from '../Tools/Header';
+import Topbar from '../Tools/Top-bar';
+import '../App.css';
+import Summary from '../Summary/Summary';
+import * as dayjs from 'dayjs';
+import { connectStomp, disconnectStomp } from './ws';
+import axios from 'axios';
+
+function Chatroom(props){
+    const [text, setText]=useState('');
+    const [chat, setChat]=useState([]);
+    const [lock, setLock]=useState(false);
+    const messageEndRef = useRef();
+  
+    const finBtn=()=>{
+      if(!lock){setLock(true)}
+    }
+  
+    const onChange=(e)=>{
+      const inputText=e.target.value;
+      setText(inputText);
+    }
+  
+    const activeEnter = (e) => {
+      if(e.key === "Enter"){
+        sendMsg();
+      }
+    }
+  
+    const sendMsg = () => {
+      let copyChat = [...chat];
+      if(text !== '' && !lock) {
+        copyChat.push(text);
+        setChat(copyChat);
+        setText('');
+      }
+    }
+  
+    useEffect(()=>{
+      messageEndRef.current.scrollIntoView({behavior: 'smooth'});
+    }, [chat]);
+    
+    return (
+      <div>
+        {lock ?
+        (
+          <Summary sidebar={props.sidebar} />
+        ) :
+        (
+          <div>
+            <button className='finbtn' onClick={finBtn}>회의 끝내기</button>
+            <div className={props.sidebar ? 'wrapper_fold' :'wrapper_unfold'}>
+              <div className={props.sidebar ? 'chat_fold' : 'chat_unfold'}>
+                <ul className='_ul'>
+                  <ChatList ChatList={chat} />
+                </ul>
+                <div ref={messageEndRef}></div>
+              </div>
+            </div>
+            <input onChange={onChange} onKeyDown={(e)=>activeEnter(e)} className={props.sidebar ? 'chat_bar_fold' :'chat_bar_unfold'} type='text' name='text' placeholder='   메세지 보내기' value={text}></input>      
+          </div>
+        )}
+      </div>
+    );
+  }
+  
+  function ChatList({ChatList}){
+    return(
+      ChatList.map((chat, i)=>{
+        return(
+          <li key={i}>
+            {chat}
+          </li>
+        );
+      })
+    );
+  }
+
+  export default Chatroom;
