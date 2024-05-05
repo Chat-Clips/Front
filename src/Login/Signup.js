b7b51962f0e9d8df71129fb3c877c18f8b349816
hyoinkangg <rkdgydls1234@gmail.com>
2024-05-06 06:44:29 +0900

    commit Signup

--- src/Login/Signup.js
+++ src/Login/Signup.js
@@ -1,28 +1,54 @@
 import { useState } from 'react';
 import './Signup.css'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Signup(){
     const navigate=useNavigate();
     const [id, setId]=useState('')
     const [pwd, setPwd]=useState('')
-    const [compwd, setCompwd]=useState('')
     const [nickname, setNickname]=useState('')
 
-    const handlesignup = event =>{
-        navigate('/Login');
+    const movetobef=()=>{
+      navigate('/Login');
     }
-//닉네임 아이디 비밀번호 비밀번호 확인
+
+    const PostSignup=async()=>{
+      let data={
+        userId: id,
+        username: nickname,
+        password: pwd
+      };
+      try{
+        const res=await axios.post("/user/signup", data);
+        console.log(data,res);
+        return res;
+      }
+      catch(error){
+        console.error(error);
+      }
+    }
+    const handlesignup = async(event) =>{
+      const res= await PostSignup();
+      if(res.data === "회원가입이 완료되었습니다."){
+        alert('회원가입 완료');
+        navigate('/Login');
+      }else{
+        alert('error');
+        setId('');setNickname('');setPwd('');
+      }
+      
+    }
+    
     return(
       <div className='signup'>
-        <button className='back_btn' onClick={handlesignup}>✘</button>
+        <button className='back_btn' onClick={movetobef}>✘</button>
         <div>Chat-Clips</div>
-        <form>
+        <form onSubmit={handlesignup}>
           <div><input type='text' className='login_content' value={nickname} placeholder='닉네임' required onChange={event => setNickname(event.currentTarget.value)}></input></div>
           <div><input type='text' className='login_content' value={id} placeholder='아이디' required onChange={event => setId(event.currentTarget.value)}></input></div>
           <div><input type='password' className='login_content' value={pwd} placeholder='비밀번호' onChange={event => setPwd(event.currentTarget.value)}></input></div>
-          <div><input type='password' className='login_content' value={compwd} placeholder='비밀번호 확인' onChange={event => setCompwd(event.currentTarget.value)}></input></div>
-          <div><button type='submit' className='login_btn' onClick={handlesignup}>회원가입</button></div>
+          <div><input type='submit' className='login_btn' value='회원가입'></input></div>
         </form>
       </div>
     );
